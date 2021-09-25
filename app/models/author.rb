4c09a8b1b66abe93a2640108e930f2d88286acb8
Ashley Follen <ashleyfollen@outlook.com>
2021-09-25 14:32:31 -0600

    completed assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    # Validations: add validations to Author such that 1. the name cannot be blank (empty/falsy), 2. the e-mail is case-sensitively unique
+    validates :name, presence: true
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    # Validations: add validations to Post such that: 
+    # 1. the title cannot be blank
+    validates :title, presence: true
+    # 2. the category is either "Fiction" or "Non-Fiction"
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    # 3. the content is at least 100 characters long
+    validates :content, length: { minimum: 100 }
 end
